4f3140800058516a7cd5e71f3d0a1cce9265c6c6
ger <german.girod@accelone.com>
2024-04-18 11:23:24 -0400

    fix secrets issue

--- chat.py
+++ chat.py
@@ -22,7 +22,7 @@ def get_response(user_query, chat_history):
 
     prompt = ChatPromptTemplate.from_template(template)
 
-    llm = ChatOpenAI()
+    llm = ChatOpenAI(openai_api_key = st.secrets["OPENAI_API_KEY"])
         
     chain = prompt | llm | StrOutputParser()
     
